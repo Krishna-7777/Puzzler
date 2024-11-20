cb05417e3626c364651efdee7490b3125fb78e3a
krishna.chhabra <krisha.chhabra@cointab.in>
2024-11-20 21:16:30 +0530

    style changes, crop for uploaded image, slide bug fix, randomizer changed

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let isHidden = true;
 let image = new Image();
 
 function script(flag, url) {
-    console.log("script");
     let randomQuery = `?${new Date().getTime()}`
     var screenWidth = window.innerWidth;
     var screenHeight = window.innerHeight;
@@ -15,7 +14,7 @@ function script(flag, url) {
     let divs = []
     var pieces = 9;
     let progress = 0
-    let notInPlace = []
+    let pieceMoved = false;
     let lastPiece;
     if (!flag)
         image.src = imageSrc;
@@ -29,6 +28,18 @@ function script(flag, url) {
     image.onload = load
 
     function load() {
+        let extraWidth = image.naturalWidth - screenWidth
+        let extraHeight = image.naturalHeight - screenHeight
+        if (extraWidth < 0)
+            extraWidth = 0
+        extraWidth = Math.floor(extraWidth / 2)
+        if (extraHeight < 0)
+            extraHeight = 0
+        extraHeight = Math.floor(extraHeight / 2)
+        if (image.naturalWidth > screenWidth)
+            image.width = screenWidth
+        if (image.naturalHeight > screenHeight)
+            image.height = screenHeight
         const tileWidth = image.width / 3;
         const tileHeight = image.height / 3;
         var id = 0
@@ -40,7 +51,7 @@ function script(flag, url) {
                 canvas.height = tileHeight;
                 canvas.id = id++
                 const ctx = canvas.getContext('2d');
-                ctx.drawImage(image, col * tileWidth, row * tileHeight, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+                ctx.drawImage(image, extraWidth + (col * tileWidth), extraHeight + (row * tileHeight), tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
                 canvas.className = "pieces"
                 if (row == 2 && col == 2) {
                     lastPiece = canvas
@@ -56,9 +67,23 @@ function script(flag, url) {
             }
         }
 
-        for (let i = pieces - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [divs[i], divs[j]] = [divs[j], divs[i]];
+        let div2 = 8, div1;
+        for (let i = 0; i < 600; i++) {
+            div1 = Math.floor(Math.random() * 8)
+            if (!(div1 == 3 && div2 == 2) && !(div1 == 6 && div2 == 5) && !(div1 == 2 && div2 == 3) && !(div1 == 5 && div2 == 6) && (div1 + 1 == div2 || div1 - 1 == div2 || div1 - 3 == div2 || div1 + 3 == div2)) {
+                let replace = divs[div1]
+                divs[div1] = divs[div2]
+                divs[div2] = replace
+                div2 = div1
+            }
+            progress = 0
+            for (let i = 0; i < pieces; i++) {
+                if (i == divs[i].id) {
+                    progress++
+                }
+            }
+            if (progress == 0)
+                break;
         }
 
         render()
@@ -74,12 +99,14 @@ function script(flag, url) {
                         return div.id == 8
                     })
 
-                    if (divClicked + 1 == emptyDiv || divClicked - 1 == emptyDiv || divClicked - 3 == emptyDiv || divClicked + 3 == emptyDiv) {
+                    if (!(divClicked == 3 && emptyDiv == 2) && !(divClicked == 6 && emptyDiv == 5) && !(divClicked == 2 && emptyDiv == 3) && !(divClicked == 5 && emptyDiv == 6) && (divClicked + 1 == emptyDiv || divClicked - 1 == emptyDiv || divClicked - 3 == emptyDiv || divClicked + 3 == emptyDiv)) {
                         let replace = divs[divClicked]
                         divs[divClicked] = divs[emptyDiv]
                         divs[emptyDiv] = replace
+                        pieceMoved = true
                     }
                     render()
+                    pieceMoved = false
                 }
             })
 
@@ -107,10 +134,12 @@ function script(flag, url) {
                 document.getElementById('puzzle').appendChild(divs[i]);
             }
         }
-        progress = Math.floor((progress / 9) * 100)
-        if (progress != 100)
-            progress += Math.floor(Math.random() * 5) + 1
-        document.getElementById("progress").innerText = progress + "%"
+        if (pieceMoved) {
+            progress = Math.floor((progress / 9) * 100)
+            if (progress != 100)
+                progress += Math.floor(Math.random() * 5) + 1
+            document.getElementById("progress").innerText = progress + "%"
+        }
     }
 }
 
